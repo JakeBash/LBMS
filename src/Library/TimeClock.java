11843e3883c100758111d8a7f8d9d61aa00581ef
tilley14 <nmt2400@g.rit.edu>
2017-03-14 13:22:27 -0400

    Added constructors to TimeClock Class
    Tests the constructor in the file

--- src/Library/TimeClock.java
+++ src/Library/TimeClock.java
@@ -1,12 +1,73 @@
 package Library;
 
 /**
- * Created by JakeDesktop on 3/13/2017.
+ *	@author Nikolas Tilley
+ * @version 0.1
  */
 public class TimeClock
 {
-    public TimeClock()
-    {
+    private int offDay ;
+    private int offHour ;
+    // Need a list of Observers to notify... or one observers
 
+	 public TimeClock()
+	 {
+	     offDay = 0 ;
+        offHour = 0 ;
+	 
+	 }
+    
+    /**
+     *
+     *
+     * @param offDay the number of days offset fromt the current date
+     * @param offHour the number of hours offset from the current time
+	  */
+	 public TimeClock(int offDay, int offHour)
+	 {
+        this.offDay = offDay ;
+        this.offHour = offHour ; 
+	 }
+    
+ 
+    // Add Days to offset
+    // Get offset number of days
+    public int getOffDay()
+    {
+        return offDay ;
     }
+    
+    
+    // Add Hours to offset
+    // Get offset number of hours
+    public int getOffHour()
+    {
+        return offHour ;
+    }
+    
+    
+    /**
+     * Notifies the list of subscribers to check the time
+     *
+     */
+    public void Notify()
+    {
+    }
+    
+    
+    
+    
+    
+    
+   public static void main (String [] args) {
+       TimeClock time = new TimeClock(2, 4) ;
+       
+       // Test 1
+       if (time.getOffHour() == 4)
+           System.out.println("PASSED Test - 1") ;
+       else
+           System.out.println("FAILED Test - 1: Expected 4, got " + time.getOffHour()) ;
+   
+   }
+    
 }
