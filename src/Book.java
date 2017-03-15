d8ddd7e69a0e43194ea02c737f05fd439f86028b
JakeBash <jbashaw36@gmail.com>
2017-03-15 13:14:12 -0400

    Added initial implementation of Books class
    Created constrcutor and getters in order to begin the initial
    implementation of BookStorage

--- src/Book.java
+++ src/Book.java
@@ -3,8 +3,81 @@
  */
 public class Book
 {
-    public Book()
-    {
+    private int isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String publishDate;
+    private int pageCount;
+    private int numCopies;
+    private int availableCopies;
 
+    public Book(int isbn, String title, String author, String publisher, String publishDate, int pageCount, int quantity)
+    {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publishDate = publishDate;
+        this.pageCount = pageCount;
+        this.numCopies = quantity;
+        availableCopies = quantity;
+    }
+
+    public int getIsbn()
+    {
+        return isbn;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getPublisher()
+    {
+        return publisher;
+    }
+
+    public String getPublishDate()
+    {
+        return publishDate;
+    }
+
+    public int getPageCount()
+    {
+        return pageCount;
+    }
+
+    public int getNumCopies()
+    {
+        return numCopies;
+    }
+
+    public int getAvailableCopies()
+    {
+        return availableCopies;
+    }
+
+    public void addCopies(int amt)
+    {
+        numCopies += amt;
+        availableCopies += amt;
+    }
+
+    public boolean checkout()
+    {
+        if(availableCopies > 0)
+        {
+            availableCopies--;
+            return true;
+        }
+        else
+            return false;
     }
 }
