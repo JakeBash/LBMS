9342d0562687eba653bd184092e9217acca502d9
tydus101 <trr4652@rit.edu>
2017-03-16 21:12:28 -0400

    Added method signitures of BookCatalog

--- src/BooksCatalog/BookCatalog.java
+++ src/BooksCatalog/BookCatalog.java
@@ -1,9 +1,15 @@
 package BooksCatalog;
+import Books.Book;
 
 /**
  * Created by JakeDesktop on 3/13/2017.
  */
 public interface BookCatalog
 {
+
+    ArrayList<Book> load(File file);
+
+    ArrayList<Book> get(String ... a);
+
 
 }
