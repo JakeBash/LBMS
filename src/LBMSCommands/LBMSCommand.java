a1bbd633e7a33239de974b7db18280e4ac0dbe02
tilley14 <nmt2400@g.rit.edu>
2017-03-19 19:34:59 -0400

    Added Comments to LBMSCommand
    annotated authors and javadoc comments

--- src/LBMSCommands/LBMSCommand.java
+++ src/LBMSCommands/LBMSCommand.java
@@ -1,7 +1,10 @@
 package LBMSCommands;
 
 /**
- * Created by JakeDesktop on 3/13/2017.
+ * Interface that gives Commands a uniform execution signature
+ *
+ * @author Jake Bashaw
+ * @author Nikolas Tilley
  */
 public interface LBMSCommand
 {
