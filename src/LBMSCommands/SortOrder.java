808ae79310db9555f3e592e44d81eeb5dbf84235
tilley14 <nmt2400@g.rit.edu>
2017-03-20 11:00:25 -0400

    SortOrder (#8)
    * Added sort Method to SortOrder Interface
    
    Updated Concrete Sort classes to reflect change
    to the SortOrder interface
    
    * Implemented sort ByTitle Strategy
    
    Implemented Comparator for sorting books by title
    Added a test to class
    
    * Changed Tests of sort ByTitle
    
    Changed the way the test is displayed
    Added comments
    
    * Implemented ByStatus Sort Class
    
    Implemented constructor and sort method for ByStatus
    Implemented tests for ByStatus, they all pass
    
    Fixed ByTitle test to use the ByTitle object and not just the comparator
    
    * Fixed some whitespace formatting
    
    * Implemented ByPublishDate sort order
    
    Did not need to use actual dates because of the consistent format
    Implemented the Constructor, comparator, and sort method
    Passes unit tests

--- src/LBMSCommands/SortOrder.java
+++ src/LBMSCommands/SortOrder.java
@@ -1,9 +1,20 @@
 package LBMSCommands;
 
+import Books.Book;
+import java.util.ArrayList;
+
+
 /**
- * Created by JakeDesktop on 3/13/2017.
+ * An interface to implement the SortOrder strategy.
+ * A user should be able to select the order they want
+ * search results returned in.
+ *
+ * Sort orders should be aggregated to Searches (the context)
+ *
+ * @author Nikolas Tilley
  */
 public interface SortOrder
 {
+    public void sort(ArrayList<Book> bookList) ;
 
 }
