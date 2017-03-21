747950f7622fe9a3ca9ae39e0b3b21eda595a41b
tilley14 <nmt2400@g.rit.edu>
2017-03-21 16:38:19 -0400

    Added Date String Format for TimeClock
    Moved unit tests to bottom of the file out of the way
    Added kyler to list of authors

--- src/Library/TimeClock.java
+++ src/Library/TimeClock.java
@@ -10,7 +10,7 @@ import java.util.Date;
  * Description
  *
  * @author Nikolas Tilley
- * @version 0.1
+ * @author Kyler Freas
  */
 public class TimeClock implements java.io.Serializable
 {
@@ -79,6 +79,41 @@ public class TimeClock implements java.io.Serializable
     }
 
     /**
+     * Formats the current date in the form YYYY/MM/DD
+     *
+     * @return a String with the  formatted date
+     */
+    public String getFormattedDate()
+    {
+        Calendar c = getCalendarDate();
+        return c.get(Calendar.YEAR) + "/" + c.get(Calendar.MONTH) + "/" + c.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * Formats the current time in the form HH:MM:SS
+     *
+     * @return a String with the formatted time
+     */
+    public String getFormattedTime()
+    {
+        Calendar c = getCalendarDate();
+        return c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
+    }
+
+    /**
+     * Formats the current date and time in the form YYYY/MM/DD,HH:MM:SS
+     *
+     * @return a String with the formatted date and time
+     */
+    public String getFormattedDateTime()
+    {
+        Calendar c = getCalendarDate();
+        return c.get(Calendar.YEAR) + "/" + c.get(Calendar.MONTH) + "/" + c.get(Calendar.DAY_OF_MONTH) + "," +
+                c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
+    }
+
+
+    /**
      * Advances the clock forward in Time
      * the number of days can be between 0 and 7,
      * the number of hours can be between 0 and 23
@@ -92,59 +127,6 @@ public class TimeClock implements java.io.Serializable
         hourOffset += hours;
     }
 
-
-    // TODO - Need to be able to save and come back from file
-
-    public static void main(String[] args)
-    {
-        TimeClock time = new TimeClock(2, 4);
-        //TimeClock time = new TimeClock();
-
-        // Test 1
-        if (time.getOffHour() == 4)
-            System.out.println("PASSED Test - 1");
-        else
-            System.out.println("FAILED Test - 1: Expected 4, got " + time.getOffHour());
-
-        // Test 2
-        time.advanceTime(1, 0);
-        if (time.getOffDay() == 3)
-            System.out.println("PASSED Test - 2");
-        else
-            System.out.println("FAILED Test - 2: Expected 3, got " + time.getOffDay());
-
-        // Test 3
-        if (time.getCurrentDateTime().compareTo(Calendar.getInstance().getTime()) > 0)
-            System.out.println("PASSED Test - 3");
-        else
-            System.out.println("FAILED Test - 3: Expected our calendar with time advanced" +
-                    "\nto be greater than the current time, got : " + time.getCurrentDateTime());
-
-
-
-        /**
-         * CAREFUL OF TESTING THIS! MIGHT OVER WRITE IMPORTANT DATA
-        // Test Serialization of Time
-        time.serialize();
-
-        TimeClock serClock = TimeClock.deserialize() ;
-        // Test 4
-        if (serClock.getOffHour() == 4)
-            System.out.println("PASSED Test - 4");
-        else
-            System.out.println("FAILED Test - 4: Expected 4, got " + deserialize().getOffHour());
-        // Test 5
-        if (serClock.getOffDay() == 3)
-            System.out.println("PASSED Test - 5");
-        else
-            System.out.println("FAILED Test - 5: Expected 3, got " + serClock.getOffDay());
-
-        System.out.println("REMEMBER TO DELETE SER AFTER TEST");
-        */
-
-
-
-    }
 
     /**
      * Serialize the time clock and save it to a text file at library shutdown
@@ -205,4 +187,61 @@ public class TimeClock implements java.io.Serializable
         // If an error occurs, return an empty storage
         return new TimeClock();
     }
+
+
+    /**
+     *
+     * Unit Tests for TimeClock
+     */
+    public static void main(String[] args)
+    {
+        TimeClock time = new TimeClock(2, 4);
+        //TimeClock time = new TimeClock();
+
+        // Test 1
+        if (time.getOffHour() == 4)
+            System.out.println("PASSED Test - 1");
+        else
+            System.out.println("FAILED Test - 1: Expected 4, got " + time.getOffHour());
+
+        // Test 2
+        time.advanceTime(1, 0);
+        if (time.getOffDay() == 3)
+            System.out.println("PASSED Test - 2");
+        else
+            System.out.println("FAILED Test - 2: Expected 3, got " + time.getOffDay());
+
+        // Test 3
+        if (time.getCurrentDateTime().compareTo(Calendar.getInstance().getTime()) > 0)
+            System.out.println("PASSED Test - 3");
+        else
+            System.out.println("FAILED Test - 3: Expected our calendar with time advanced" +
+                    "\nto be greater than the current time, got : " + time.getCurrentDateTime());
+
+
+        System.out.println(time.getFormattedDateTime());
+        System.out.println(time.getFormattedDate());
+        System.out.println(time.getFormattedTime());
+
+        /**
+         * CAREFUL OF TESTING THIS! MIGHT OVER WRITE IMPORTANT DATA
+        // Test Serialization of Time
+        time.serialize();
+
+        TimeClock serClock = TimeClock.deserialize() ;
+        // Test 4
+        if (serClock.getOffHour() == 4)
+            System.out.println("PASSED Test - 4");
+        else
+            System.out.println("FAILED Test - 4: Expected 4, got " + deserialize().getOffHour());
+        // Test 5
+        if (serClock.getOffDay() == 3)
+            System.out.println("PASSED Test - 5");
+        else
+            System.out.println("FAILED Test - 5: Expected 3, got " + serClock.getOffDay());
+
+        System.out.println("REMEMBER TO DELETE SER AFTER TEST");
+        */
+    }
+
 }
