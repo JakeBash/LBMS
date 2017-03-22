402fc9359376b4a2d09f6252dbfdfb67221599f2
FreasK <kylerf@hughes.net>
2017-03-22 03:17:27 -0400

    more cereal

--- src/Visitors/CheckOut.java
+++ src/Visitors/CheckOut.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
  *
  * @author Kyler Freas
  */
-public class CheckOut
+public class CheckOut implements java.io.Serializable
 {
     private Book book;
     private Calendar borrowDate;
