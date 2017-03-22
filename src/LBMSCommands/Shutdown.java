661daad22722d029a2f95d23eb0afac87ceb77f7
tilley14 <nmt2400@g.rit.edu>
2017-03-22 02:45:46 -0400

    Added Shutdown command

---
+++ src/LBMSCommands/Shutdown.java
@@ -0,0 +1,34 @@
+package LBMSCommands;
+
+import Library.Library;
+
+/**
+ * Displays the current date and time in the simulation. This should include any days that have been added to the
+ * calendar using the command to advance time.
+ *
+ * Command format: datetime;
+ *
+ * @author Nikolas Tilley
+ */
+public class Shutdown implements LBMSCommand
+{
+    private Library library;
+
+    /**
+     *  Creates a new GetTime command object.
+     *
+     * @param library - The clock keeping track of the date and time.
+     */
+    public Shutdown(Library library)
+    {
+        this.library = library;
+    }
+
+    /**
+     * Executes the GetTime command on the library.
+     */
+    public void execute()
+    {
+        library.shutdown();
+    }
+}
